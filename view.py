07b4e73034f2fdc52932d450ee8518f1ec3dee03
yasshy-koya <159101504+yasshy-koya@users.noreply.github.com>
2024-05-20 20:01:31 +0900

    Add files via upload

---
+++ view.py
@@ -0,0 +1,19 @@
+#codeing: utf-8
+
+from flask import Flask, render_template
+
+#Flaskをインスタンス化
+app = Flask(__name__)
+
+#___view側の設定＿＿＿
+#ルートディレクトリにアクセスした場合の挙動
+@app.route('/')
+#def以下がアクセス後の操作
+def index():
+    #return 'Hello World だぞ!'
+    return render_template("index.html")
+
+#エントリーポイント
+if __name__=='__main__':
+    app.run()
+
